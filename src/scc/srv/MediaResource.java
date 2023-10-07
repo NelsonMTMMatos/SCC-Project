ac2194814b28a444e3fb252d3d26f77c708efb28
Nélson <nm.matos@campus.fct.unl.pt>
2023-10-07 19:51:37 +0100

    Server related classes

---
+++ src/scc/srv/MediaResource.java
@@ -0,0 +1,90 @@
+package scc.srv;
+
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
+import scc.utils.Hash;
+
+import java.util.List;
+
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+/**
+ * Resource for managing media files, such as images.
+ */
+@Path("/media")
+public class MediaResource
+{
+	String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=scc60483;AccountKey=t03lDK7vEg/Pa1He1cgCh9ULjHZIwaJwMEZ8URGMBkFIbhB8ueGEPK3XsNqQj45t4Y7OP1wZ8RjG+ASty4Onpg==;EndpointSuffix=core.windows.net";
+
+	BlobContainerClient containerClient;
+
+	MediaResource(){
+		containerClient = new BlobContainerClientBuilder()
+				.connectionString(storageConnectionString)
+				.containerName("images")
+				.buildClient();
+	}
+	/**
+	 * Post a new image.The id of the image is its hash.
+	 */
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String upload(byte[] contents) {
+		String filename = Hash.of(contents) + ".jpg";
+		try{
+			BlobClient blob = containerClient.getBlobClient(filename);
+
+			blob.upload(BinaryData.fromBytes(contents));
+
+			System.out.println( "File updloaded : " + filename);
+		}catch( Exception e) {
+			e.printStackTrace();
+		}
+
+		return filename;
+	}
+
+	/**
+	 * Return the contents of an image. Throw an appropriate error message if
+	 * id does not exist.
+	 */
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public byte[] download(@PathParam("id") String id) {
+		try{
+			BlobClient blob = containerClient.getBlobClient(id);
+
+			if(!blob.exists()) throw new NotFoundException();
+
+			return blob.downloadContent().toBytes();
+		}catch( Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Lists the ids of images stored.
+	 */
+	@GET
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<String> list() {
+		return containerClient.listBlobs().stream().map((BlobItem::getName)).toList();
+	}
+
+}
